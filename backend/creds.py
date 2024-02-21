493ff6b9d8fd0036eff549c89328458de380a943
temez <temilolu.olufore@gmail.com>
2024-02-21 10:48:44 -0600

    LSSF Application Complete Temiloluwa

---
+++ backend/creds.py
@@ -0,0 +1,6 @@
+class Creds:
+    def __init__(self):
+        self.conString = "localhost"
+        self.userName = "root"
+        self.password = "testing123"
+        self.dbName = "LSSF"
---
+++ frontend/create_hash.js
@@ -0,0 +1,11 @@
+// example script for hash generation
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const password = 'hello';
+
+bcrypt
+  .hash(password, saltRounds)
+  .then((hash) => {
+    console.log('Hash: ', hash);
+  })
+  .catch((err) => console.error(err.message));
