de5c8144aaaf40799d028413ce77d1c5ebd2ad75
temez <temilolu.olufore@gmail.com>
2024-03-29 22:07:55 -0500

    MySQL to Microsoft Azure SQL Server Migration

--- backend/api.py
+++ backend/api.py
@@ -1,23 +1,25 @@
-#Temiloluwa Olufore 1970625
+#Temiloluwa Olufore
 
 import flask
-from flask import Flask, request, jsonify, make_response
-import creds
+from flask import Flask, request, jsonify
 from sqlhelper import create_connection, execute_read_query, execute_query #class code functions imported
 import os
-
+from dotenv import load_dotenv
 
 app = flask.Flask(__name__) #Flask setup
 app.json.sort_keys = False
+load_dotenv()
 
-conString = os.getenv("CON_STRING")
-userName = os.getenv("USER_NAME")
+
+conString = os.getenv("SERVER")
+userName = os.getenv("USERNAME")
 password = os.getenv("PASSWORD")
-dbName = os.getenv("DB_NAME")
+dbName = os.getenv("DATABASE")
+driver = os.getenv("DRIVER")
 
 @app.route('/api/login', methods=['GET']) 
 def get_hash():
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     sql = "SELECT HashesforDecrypt FROM Hashes WHERE idHashes = 1;" #hash stored in db for frontend login
     loginhash = execute_read_query(conn, sql)
     return jsonify(loginhash)
@@ -26,7 +28,7 @@ def get_hash():
 #FLOOR CRUDs
 @app.route('/api/floor', methods=['POST']) #creating floors
 def add_floor():
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     new_floor = request.json
     sqlfloor = "SELECT * FROM floor;"
     floorlevel = execute_read_query(conn, sqlfloor)
@@ -38,22 +40,23 @@ def add_floor():
     for name in names:
         if name == new_floor["name"]: 
             return jsonify({"Message": "This floor name already exists"})
-    sql = "INSERT INTO floor(level, name) VALUES (%s, %s)"
+    sql = "INSERT INTO [floor] ([level], [name]) VALUES (?, ?)"
     values = (new_floor["level"], new_floor["name"])
-    floor = execute_query(conn, sql, values)
-    levels.append(new_floor["level"]) #Chat GPT updating my levels list with every iteration
-    return jsonify(floor) #Jsonify will return data relevant to its table "floor", "room", "resident"
+    execute_query(conn, sql, values)
+    levels.append(new_floor["level"])
+    return jsonify({"message": "Floor added successfully"})  # Assuming you just want to return a success message
+
 
 @app.route('/api/floor', methods=['GET']) #views all floors
 def view_floor():
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     sql = "SELECT level, name FROM floor;" #selecting the variables only (No Id)
     floor = execute_read_query(conn, sql)
     return jsonify(floor)
 
 @app.route('/api/floor/<floorlevel>', methods=['PUT']) #Updating the floor via name and level (done in line with local url)
 def update_floor(floorlevel):
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     floor_update = request.json
     sqlfloor = "SELECT * FROM floor;" #Queries so that data is the latest for each request
     floorname = execute_read_query(conn, sqlfloor)
@@ -61,18 +64,18 @@ def update_floor(floorlevel):
     for name in names:
         if name == floor_update["name"]: 
             return jsonify({"Message": "This floor name already exists"})
-    sql = "UPDATE floor SET name = %s WHERE level = %s" #updating the name of the floor at level
+    sql = "UPDATE [floor] SET [name] = ? WHERE [level] = ?" #updating the name of the floor at level
     values = (floor_update["name"], floorlevel)
     floor = execute_query(conn, sql, values)
     return jsonify({"Message": "Floor name updated successfully"})
 
 @app.route('/api/floor/<level>', methods=['DELETE']) #floor deletion 
 def delete_floor(level):
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     sqlfloor = "SELECT * FROM floor;"
     floorlevel = execute_read_query(conn, sqlfloor)
     levels = [i['level'] for i in floorlevel]
-    sql = "DELETE FROM floor WHERE level = %s" 
+    sql = "DELETE FROM [floor] WHERE [level] = ?" 
     floor = execute_query(conn, sql, (level,)) #chat gpt tuple for the third column as strings can't be passed for deletion
     return jsonify({"Message": "Floor deleted sucessfully"})
 #FLOOR CRUDs END
@@ -83,7 +86,7 @@ def delete_floor(level):
 #ROOM CRUDs
 @app.route('/api/room', methods=['POST'])
 def add_room():
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     new_room = request.json
     sqlroom = "SELECT * FROM room;" #Queries so that data is the latest for each request
     sqlfloor = "SELECT * FROM floor;"
@@ -98,12 +101,12 @@ def add_room():
         if int(rooms) == int(new_room["number"]): #chat gpt added int to check for duplicate levels and also the formatting and the error message
             return jsonify({"Message": "This room number already  exists"})
     if int(new_room["floor"]) in levels and new_room["number"][0] == new_room["floor"]:
-        sql = "INSERT INTO room (number, capacity, floor) VALUES (%s, %s, (SELECT id FROM floor WHERE level = %s));"
+        sql = "INSERT INTO [room] ([number], [capacity], [floor]) VALUES (?, ?, (SELECT id FROM floor WHERE level = ?));"
         values = (new_room["number"], new_room["capacity"], new_room["floor"])
         room = execute_query(conn, sql, values)
         return jsonify(room)
     elif int(new_room["floor"]) in levels and negint == new_room["floor"]:
-        sql = "INSERT INTO room (number, capacity, floor) VALUES (%s, %s, (SELECT id FROM floor WHERE level = %s));"
+        sql = "INSERT INTO [room] ([number], [capacity], [floor]) VALUES (?, ?, (SELECT id FROM floor WHERE level = ?));"
         values = (new_room["number"], new_room["capacity"], new_room["floor"])
         room = execute_query(conn, sql, values)
         return jsonify(room)
@@ -113,7 +116,7 @@ def add_room():
 
 @app.route('/api/room', methods=['GET']) #views all rooms
 def view_room():
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     sql = """SELECT ro.number, ro.capacity, f.level as floor
                 FROM room ro
                 JOIN floor f ON ro.floor = f.id
@@ -124,9 +127,9 @@ def view_room():
 
 @app.route('/api/room/<roomnumber>', methods=['PUT']) #Updating capacity of rooms
 def update_room(roomnumber):
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     room_update = request.json
-    sql = "UPDATE room SET capacity = %s WHERE number = %s" 
+    sql = "UPDATE [room] SET [capacity] = ? WHERE number = ?" 
     values = (room_update["capacity"], roomnumber)
     room = execute_query(conn, sql, values)
     return jsonify({"Message": "Room capacity updated successfully"})
@@ -135,20 +138,20 @@ def update_room(roomnumber):
 
 @app.route('/api/room/<number>', methods=['DELETE']) #room deletion using parameters
 def delete_room(number):
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     sqlroom = "SELECT * FROM room;"
     roomquery = execute_read_query(conn, sqlroom)
     room_numbers = [i['number'] for i in roomquery]
     sql3 = """SELECT firstname, lastname
             FROM resident
-            WHERE room = (SELECT `id` FROM `room` WHERE `number` = %s);"""
+            WHERE room = (SELECT id FROM room WHERE number = ?);"""
     values = (number,)
     residents = execute_read_query(conn, sql3, (number,)) #Resident checking
     names = [i['lastname'] for i in residents ]
     if len(names) > 0:
         return jsonify({"Message": "Cannot delete currently occupied room."})  #Residents will not be deleted if they are still in room
     else:
-        sql = "DELETE FROM room WHERE number = %s" 
+        sql = "DELETE FROM [room] WHERE number = ?" 
         room = execute_query(conn, sql, (number,)) #chat gpt tuple for the third column as strings can't be passed for deletion
         return jsonify({"Message": "Room deleted sucessfully"})    
 #ROOM CRUDs END
@@ -159,7 +162,7 @@ def delete_room(number):
 # RESIDENT CRUDs
 @app.route('/api/resident', methods=['POST']) #creating residents 
 def add_resident():
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     new_resident = request.json
     sqlcap = "SELECT number, capacity FROM room;"
     capacities = execute_read_query(conn, sqlcap) #capacity checking
@@ -168,12 +171,13 @@ def add_resident():
             roomcap = i
     if len([new_resident["residentID"]][0]) < 3:
         return jsonify({"Message": "Please generate a Resident ID"})
-    sql = """INSERT INTO resident(residentID, firstname, lastname, age, room) VALUES (%s, %s, %s, %s, (SELECT `id` FROM `room` WHERE `number` = %s));""" #select the room number that will automatically match the foreign keys
+    sql = """INSERT INTO [resident] ([residentID], [firstname], [lastname], [age], [room]) VALUES (?, ?, ?, ?, (SELECT id FROM room WHERE number = ?));""" #select the room number that will automatically match the foreign keys
+    # sql = "INSERT INTO [room] ([number], [capacity], [floor]) VALUES (?, ?, (SELECT id FROM floor WHERE level = ?));"
     values = (new_resident["residentID"], new_resident["firstname"], new_resident["lastname"], new_resident["age"], new_resident["room"])
     resident = execute_query(conn, sql, values)
-    sqlroom = """UPDATE room
-                SET capacity = capacity - 1
-                WHERE number = %s"""
+    sqlroom = """UPDATE [room]
+                SET [capacity] = [capacity] - 1
+                WHERE [number] = ?"""
     values2 = (new_resident["room"],)
     room_num_cap = execute_query(conn, sqlroom, values2)
     return jsonify(resident)
@@ -181,47 +185,45 @@ def add_resident():
 
 @app.route('/api/resident', methods=['GET']) #views all residents
 def view_resident():
-    conn = create_connection(conString, userName, password, dbName)
-    sql = """SELECT r.residentID, r.firstname, r.lastname, r.age, ro.number as `room` 
+    conn = create_connection(conString, userName, password, dbName, driver)
+    sql = """SELECT r.residentID, r.firstname, r.lastname, r.age, ro.number as room
             FROM resident r
             JOIN room ro ON r.room = ro.id
             ORDER BY r.lastname ASC;"""
     resident = execute_read_query(conn, sql)
     return jsonify(resident)
 
-
-
 @app.route('/api/resident/<residentID>', methods=['PUT']) 
 def update_resident(residentID):
-    conn = create_connection(conString, userName, password, dbName)
+    conn = create_connection(conString, userName, password, dbName, driver)
     resident_update = request.json
-    sqlroom = """UPDATE room
-                SET capacity = capacity + 1
-                WHERE id = (SELECT room from resident WHERE residentID = %s)""" #Adding capacity back to the room being vacated (will be done in PUT POST and DELETES in relevan)
+    sqlroom = """UPDATE [room]
+                SET [capacity] = [capacity] + 1
+                WHERE id = (SELECT room from resident WHERE residentID = ?)""" #Adding capacity back to the room being vacated (will be done in PUT POST and DELETES in relevan)
     values2 = (residentID,)
     room_num_cap2 = execute_query(conn, sqlroom, values2)
     sql = """UPDATE resident
-            SET room = (SELECT `id` FROM `room` WHERE `number` = %s)
-            WHERE residentID = %s AND room = (SELECT `room` FROM (SELECT * FROM `resident`) AS temp 
-            WHERE residentID = %s);"""
+            SET room = (SELECT id FROM room WHERE number = ?)
+            WHERE residentID = ? AND room = (SELECT room FROM (SELECT * FROM resident) AS temp 
+            WHERE residentID = ?);"""
     values = (resident_update["room"], residentID, residentID) #resident_update["firstname"], resident_update["lastname"])
     resident = execute_query(conn, sql, values)
-    sqlroom2 = """UPDATE room
-                SET capacity = capacity - 1
-                WHERE number = %s""" # New room assingment capacity reduction
+    sqlroom2 = """UPDATE [room]
+                SET [capacity] = capacity - 1
+                WHERE [number] = ?""" # New room assingment capacity reduction
     values3 = (resident_update["room"],)
     room_num_cap = execute_query(conn, sqlroom2, values3)
     return jsonify({"Message": "Resident moved successfully"})  
 
 @app.route('/api/resident/<residentID>', methods=['DELETE']) #resident deletion
 def delete_resident(residentID):
-    conn = create_connection(conString, userName, password, dbName)
-    sql = "DELETE FROM resident WHERE residentID = %s;" 
-    sqlroom = """UPDATE room
-                SET capacity = capacity + 1
+    conn = create_connection(conString, userName, password, dbName, driver)
+    sql = "DELETE FROM [resident] WHERE [residentID] = ?;" 
+    sqlroom = """UPDATE [room]
+                SET [capacity] = capacity + 1
                 WHERE id = (SELECT r.room
                 FROM resident r
-                WHERE r.residentID = %s);"""
+                WHERE r.residentID = ?);"""
     values2 = (residentID,)
     room_num_cap = execute_query(conn, sqlroom, values2) #updating capacity after deletion
     resident = execute_query(conn, sql, (residentID,)) #chat gpt tuple for the third column as strings can't be passed for deletion
--- backend/sqlhelper.py
+++ backend/sqlhelper.py
@@ -1,40 +1,54 @@
-import mysql.connector
-from  mysql.connector import Error
+# import mysql.connector
+# from  mysql.connector import Error
+import pyodbc
+from pyodbc import Error
 
-def create_connection(host_name, user_name, user_password, db_name):
+def create_connection(server, username, password, database, driver):
     connection = None
+    conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
     try:
-        connection = mysql.connector.connect(
-            host =  host_name,
-            user = user_name,
-            password = user_password,
-            database = db_name
-        )
-    except Error as e:
+        connection = pyodbc.connect(conn_str)
+    except Exception as e:
         print(f"The error '{e}' has occured")
     return connection
 
 def execute_query(connection, query, values=None):
-    cursor = connection.cursor(dictionary=True)
+    cursor = connection.cursor()
     try:
         if values:
-            cursor.execute(query, values) #chat gpt aided values to be able to insert values into the table using POST
-            connection.commit()
+            cursor.execute(query, values)
         else:
             cursor.execute(query)
-            connection.commit()
+        connection.commit()
         print("Query executed successfully")
-    except mysql.connector.Error as e:
+    except Exception as e:
+        print(f"The error '{e}' has occurred")
+        raise  # Raise the exception to be handled by the caller
+    finally:
+        cursor.close()  # Close the cursor to release resources
+
+
+
+def execute_read_query(connection, query, values=None):
+    cursor = connection.cursor()
+    result = None
+    try:
+        if values:
+            cursor.execute(query, values)  # Execute query with provided values
+        else:
+            cursor.execute(query)  # Execute query without values
+        rows = cursor.fetchall()
+        
+        # Convert each Row object to a dictionary
+        result = []
+        for row in rows:
+            row_dict = {}
+            for idx, column in enumerate(cursor.description):
+                row_dict[column[0]] = row[idx]
+            result.append(row_dict)
+        
+        return result
+    except Exception as e:
         print(f"The error '{e}' has occurred")
 
-
-def execute_read_query(connection, query, values=None): #added a values argument to be able to parse through queries
-    cursor = connection.cursor(dictionary=True)
-    result =  None
-    try:
-        cursor.execute(query, values)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-         print(f"The error '{e}' has occured")
             
